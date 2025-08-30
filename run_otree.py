811731ed4e61e2d06898c774c270a5c21cc19aed
Rui16314 <rui315@yorku.ca>
2025-08-29 21:38:54 -0400

    Update run_otree.py

--- run_otree.py
+++ run_otree.py
@@ -1,10 +1,9 @@
 # run_otree.py
 import os, sys
-# Classic oTree entrypoint available in all v5 releases
 from otree.main import execute_from_command_line
 
 port = os.environ.get("PORT", "8000")
-# Equivalent to:  otree prodserver 1 0.0.0.0:<PORT>
-sys.argv = ["otree", "prodserver", "1", f"0.0.0.0:{port}"]
+# Start oTree like:  otree prodserver 0.0.0.0:<PORT>
+sys.argv = ["otree", "prodserver", f"0.0.0.0:{port}"]
 execute_from_command_line()
 
