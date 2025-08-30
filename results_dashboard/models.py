2c209367ec3a7092f33d89344eeeca4166825805
Rui16314 <rui315@yorku.ca>
2025-08-29 22:34:55 -0400

    Update models.py

--- results_dashboard/models.py
+++ results_dashboard/models.py
@@ -1,5 +1,12 @@
 from otree.api import *
-class C(BaseConstants): NAME_IN_URL='results_dashboard'; PLAYERS_PER_GROUP=None; NUM_ROUNDS=1
+
+class C(BaseConstants):
+    NAME_IN_URL = 'results_dashboard'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
 class Subsession(BaseSubsession): pass
 class Group(BaseGroup): pass
-class Player(BasePlayer): bin_size=IntegerField(initial=10,min=1,max=100); target_v=IntegerField(initial=50,min=0,max=100)
+class Player(BasePlayer):
+    bin_size = models.IntegerField(initial=10, min=1, max=100)
+    target_v = models.IntegerField(initial=50, min=0, max=100)
